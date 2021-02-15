3f6fc46e9a5d7766aebc18bdabeb6f92393ff30f
Jbansal10 <jbansal@academytrainee.com>
2021-02-15 12:50:09 +0000

    [QAC-33] navbar testing completed

--- test/NavBarTest.scala
+++ test/NavBarTest.scala
@@ -1,15 +1,16 @@
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.chrome.ChromeDriver
 import org.scalatest._
 import org.scalatestplus.selenium._
-
 import org.scalatest.matchers._
 
 
-class NavBarTest extends flatspec.AnyFlatSpec with should.Matchers with HtmlUnit {
+class NavBarTest extends flatspec.AnyFlatSpec with BeforeAndAfter with should.Matchers with HtmlUnit {
 
-  val host = "http://localhost:9000"
+  val host = "http://localhost:9000/"
 
   "clicking on homepage in navbar" should "take you to homepage" in {
-    go to (host + "/")
+    go to host
     click on xpath("/html/body/nav/div/div/ul/li[1]/a")
     pageTitle should be ("Welcome to Play")
   }

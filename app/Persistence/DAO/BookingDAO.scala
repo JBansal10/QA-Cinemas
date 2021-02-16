f8fa022ecd32b6705aebc96ab6f34ffc3bbba400
Jbansal10 <jbansal@academytrainee.com>
2021-02-16 11:33:07 +0000

    [QAC-22] Booking form domain completed, DAO file created

--- app/Persistence/DAO/BookingDAO.scala
+++ app/Persistence/DAO/BookingDAO.scala
@@ -1,5 +1,16 @@
 package Persistence.DAO
 
+import Persistence.Domain.BookingFormOBJ.Booking
+import slick.jdbc.MySQLProfile.backend.Database
+import slick.lifted.TableQuery
+
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import slick.jdbc.MySQLProfile.api._
+
 object BookingDAO {
+
+  lazy val db = Database.forConfig("mysqlDB")
+  lazy val bookingTable = TableQuery[Booking]
 
 }

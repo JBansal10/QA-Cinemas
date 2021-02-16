b9bb8a83d70f94ce343403cf639726e0740de75d
ayub <ayusuf@academytrainee.com>
2021-02-16 14:18:52 +0000

    [QAC-23] AY: Added PaymentDAO and PaymentDomain

---
+++ app/Persistence/DAO/PaymentDAO.scala
@@ -0,0 +1,22 @@
+package Persistence.DAO
+
+import Persistence.Domain.paymentObj.{Payment, Payments}
+import slick.jdbc.MySQLProfile.api._
+import slick.jdbc.MySQLProfile.backend.Database
+import slick.lifted.TableQuery
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+object PaymentDAO {
+
+  lazy val db = Database.forConfig("mysqlDB")
+  lazy val paymentTable: TableQuery[Payments] = TableQuery[Payments]
+
+  def create(pay: Payment): Future[String] = {
+    db.run(paymentTable += pay).map(res => "payment details submitted successfully").recover{
+      case ex: Exception =>
+        ex.getCause.getMessage
+    }
+  }
+}

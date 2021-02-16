f006aab52a0f8f9ce84da8d758dfabc5bf52e698
Jbansal10 <jbansal@academytrainee.com>
2021-02-16 10:55:36 +0000

    [QAC-22] Booking form domain completed, DAO file created

--- app/Persistence/Domain/BookingFormOBJ.scala
+++ app/Persistence/Domain/BookingFormOBJ.scala
@@ -7,19 +7,20 @@ import slick.jdbc.MySQLProfile.api._
 
 object BookingFormOBJ {
 
-  case class Booking(id: Int, mID: Int, screenDate: String, cName: String, nSeats: Int, adults: Int, childs: Int, concession: String)
+  case class Booking(id: Int, mID: Int, screenDate: String, screenTime: String, cName: String, nSeats: Int, adults: Int, childs: Int, concession: String)
 
   case class Bookings(tag: Tag) extends Table[Booking] (tag, "booking") {
     def id = column[Int]("FORM_ID", O.AutoInc, O.PrimaryKey)
     def mID = column[Int]("MOVIE_DESC")
-    def screenDateTime = column[String]("SCREEN_DATETIME")
+    def screenDate = column[String]("SCREEN_DATE")
+    def screenTime = column[String]("SCREEN_TIME")
     def cName = column[String]("CUSTOMER_NAME")
     def nSeats = column[Int]("NUMBER_OF_SEATS")
     def adults = column[Int]("ADULTS")
     def childs = column[Int]("CHILDS")
     def concession = column[String]("Concession")
 
-    def * = (id, mID, screenDateTime, cName, nSeats, adults, childs, concession) <> (Booking.tupled, Booking.unapply)
+    def * = (id, mID,screenDate, screenTime, cName, nSeats, adults, childs, concession) <> (Booking.tupled, Booking.unapply)
   }
 
   object bookingForm {
@@ -27,7 +28,8 @@ object BookingFormOBJ {
       mapping (
         "id" -> number,
         "mID" -> number,
-        "screenDateTime" -> nonEmptyText,
+        "screenDate" -> nonEmptyText,
+        "screenTime" -> nonEmptyText,
         "cName" -> nonEmptyText,
         "nSeats" -> number,
         "adults" -> number,

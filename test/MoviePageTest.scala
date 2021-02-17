3a5a121070e0c8dcd6c50e6ea6bf61426c796ca7
Simon Forster <sforster@academytrainee.com>
2021-02-17 12:45:38 +0000

    [QAC-16] Created selenium testing for movie page (SF)

---
+++ test/MoviePageTest.scala
@@ -0,0 +1,30 @@
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.htmlunit.HtmlUnitDriver
+import org.scalatest.BeforeAndAfter
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatestplus.selenium.WebBrowser
+
+import java.util.concurrent.TimeUnit
+
+class MoviePageTest extends AnyFlatSpec with WebBrowser with BeforeAndAfter with Matchers{
+
+  val host = "http://localhost:9000/"
+
+  implicit val webDriver: WebDriver = new HtmlUnitDriver()
+  webDriver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS)
+
+  behavior of "Movie page"
+
+  it should "be able to be navigated to from the listings page" in {
+      go to host + "/listgallery"
+      click on id("Titanic poster")
+      pageTitle should be ("Titanic")
+  }
+
+  it should "show the correct movie picture and screen times" in {
+    go to host + "/movie/1"
+    assert(find(xpath("/html/body/div/div/div[1]/img")).get.attribute("src") == Some(host + "assets/images/titanic.png")
+      && find(xpath("/html/body/div/div/div[2]/div/div/table/tbody/tr[2]/td")).get.text.contains("20:15 (Deluxe)"))
+  }
+}

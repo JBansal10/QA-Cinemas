f6379c0e1191def6fbdbfba0c98a1572dfdfe08f
Jbansal10 <jbansal@academytrainee.com>
2021-02-18 11:51:57 +0000

    [QAC-29] Discussion DAO create tested, admin page created

--- test/Persistence/DiscussionBoardDBUnitTest.scala
+++ test/Persistence/DiscussionBoardDBUnitTest.scala
@@ -4,13 +4,16 @@ import Persistence.DAO.DiscussionBoardDAO
 import Persistence.Domain.DiscussionBoardOBJ.DiscussionBoard
 import org.scalatest.BeforeAndAfter
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
 import slick.jdbc.H2Profile.api._
 import slick.jdbc.MySQLProfile.backend.Database
 
+import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
 import scala.io.Source
 
-class DiscussionBoardDBUnitTest extends AnyFlatSpec with BeforeAndAfter {
+class DiscussionBoardDBUnitTest extends AnyFlatSpec with BeforeAndAfter with Matchers {
 
   lazy val db = Database.forConfig("mysqlDB")
 
@@ -24,6 +27,12 @@ class DiscussionBoardDBUnitTest extends AnyFlatSpec with BeforeAndAfter {
 //  it should "create a discussion board object and pass into db" in {
 //    val discBoard : DiscussionBoard = DiscussionBoard(3, "This is test content", 2, 6)
 //  }
+
+  "DAO" should "return Right" in {
+    val dBoard: DiscussionBoard = new DiscussionBoard(3, "This is test data", "15/02/2021 14:28", 3, 8, false)
+    val result = Await.result(DiscussionBoardDAO.create(dBoard), Duration.Inf)
+    assert(result === "Discussion successfully added")
+  }
 
   it should "return the values when readAll is called" in {
     DiscussionBoardDAO.readAll() map { results =>

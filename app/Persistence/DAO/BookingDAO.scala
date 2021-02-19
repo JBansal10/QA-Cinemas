90446a0e0d64be36f90d4e6a06d326b1fea80d85
Jbansal10 <jbansal@academytrainee.com>
2021-02-19 12:35:30 +0000

    [QAC-22] DAO implementation added

--- app/Persistence/DAO/BookingDAO.scala
+++ app/Persistence/DAO/BookingDAO.scala
@@ -28,8 +28,13 @@ object BookingDAO {
   def getLastIndex(): Future[Int] = db.run(bookingTable.size.result)
 
   def totalPrice(id: Int, adults: Int, childs: Int): Future[BigDecimal] = {
-    val bidDec: BigDecimal = 9.82
-    db.run(movieTable.filter(_.id === id).result.headOption).
+    val bidDec: BigDecimal = 999.99
+    db.run(movieTable.filter(_.id === id).result.headOption).map{ movie =>
+      movie.get.aPrice * adults + movie.get.cPrice * childs
+    }recover {
+      case exception: Exception => exception.printStackTrace();
+        bidDec
+    }
   }
 
 }

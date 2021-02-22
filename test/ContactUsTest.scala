a02e5a0548164fbe7eb36326b074e49f7c35247a
Simon Forster <sforster@academytrainee.com>
2021-02-22 12:52:31 +0000

    [QAC-30] Email selenium tests completed (SF)

--- test/ContactUsTest.scala
+++ test/ContactUsTest.scala
@@ -10,33 +10,29 @@ import java.util.concurrent.TimeUnit
 class ContactUsTest extends flatspec.AnyFlatSpec with BeforeAndAfter with should.Matchers with WebBrowser {
 
   val host = "http://localhost:9000/"
-  val ContactUs = "http://localhost:9000/ContactUs"
-  implicit val webDriver: WebDriver = new HtmlUnitDriver()
+  implicit val webDriver: WebDriver = new HtmlUnitDriver(true)
   webDriver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS)
 
   "clicking on Contact Us in footer" should "take you to Contact Us page" in {
     go to host
-    click on id("contactUsFooter")
-    pageTitle should be ("Welcome to Play")
+    click on xpath("/html/body/footer/div[1]/div/div[3]/ul/li[3]/a")
+    pageTitle should be ("Contact Us")
   }
 
   "clicking on Contact Us in navbar" should "take you to Contact Us page" in {
     go to host
     click on id("aboutusDropdown")
     click on id("contactUsHeader")
-    pageTitle should be ("Welcome to Play")
+    pageTitle should be ("Contact Us")
   }
 
   "Typing on Contact Us form and Submitting" should "Send email to specified email address" in{
-    go to host
-    go to ContactUs
-    click on xpath("/html/body/div/div/div/div/form/div[1]/div/input")
-    enter ("Jake")
-    click on id("Email")
-    enter ("Jakereid@@hotmail.co.uk")
-    click on id ("Message")
-    enter ("Hello")
+    go to host + "contactus"
+    textField(id("name")).value = "Test Person"
+    emailField(id("from")).value = "test@email.com"
+    textArea(id("content")).value = "Words, words words words test words."
     submit()
+    pageTitle should be ("Email sent")
   }
 
 }

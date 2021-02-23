da2ad2a04aedaea02c6f9fcb829b103a7fafa421
Jbansal10 <jbansal@academytrainee.com>
2021-02-23 15:41:53 +0000

    [QAC-29] DiscBoard DAO tested

--- test/Schema/Schemas.scala
+++ test/Schema/Schemas.scala
@@ -23,10 +23,10 @@ object Schemas {
   )
 
   def insertData: DBIO[Unit] = DBIO.seq(
-    sqlu"INSERT INTO movie values (1, 'Titanic', 123, 'Sad', 3, 'Fish probably', 'Jeff Bezos', 'titanic.png', 'Boat drowning', 4.95, 10.95, 1)",
-    sqlu"INSERT INTO movie values (2, 'Nemo', 432, 'Scary', 5, 'Fish', 'Steve Erwin', 'Nemo.png', 'Fish looking for fish', 8.95, 6.95, 1)",
-    sqlu"INSERT INTO movie values (3, 'Toy Story', 2000, 'Happy', 6, 'Toys', 'Guy Fieri', 'ToyStory.png', 'Toys running away from their owner and doing other stuff', 9.95, 7.95, 1)",
-    sqlu"INSERT INTO movie values (4, 'Transformers', 2002, 'Epic', 7, 'Robots', 'Human', 'Transformers.png', 'Robots fighting other robots and humans are present', 8.95, 7.95, 1)",
+    sqlu"INSERT INTO movie values (1, 'Titanic', 123, 'Sad', 3, 'Fish probably', 'Jeff Bezos', 'titanic.png', 'Boat drowning', 4.95, 10.95, true)",
+    sqlu"INSERT INTO movie values (2, 'Nemo', 432, 'Scary', 5, 'Fish', 'Steve Erwin', 'Nemo.png', 'Fish looking for fish', 8.95, 6.95, true)",
+    sqlu"INSERT INTO movie values (3, 'Toy Story', 2000, 'Happy', 6, 'Toys', 'Guy Fieri', 'ToyStory.png', 'Toys running away from their owner and doing other stuff', 9.95, 7.95, true)",
+    sqlu"INSERT INTO movie values (4, 'Transformers', 2002, 'Epic', 7, 'Robots', 'Human', 'Transformers.png', 'Robots fighting other robots and humans are present', 8.95, 7.95, true)",
 
     sqlu"INSERT INTO screentime values(1, 1, 'Monday', '19:30', 'Standard')",
     sqlu"INSERT INTO screentime values(2, 1, 'Monday', '21:00', 'Standard')",

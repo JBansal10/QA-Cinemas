e952871fca13fed7b4f80af9da725b6c1715162a
Simon Forster <sforster@academytrainee.com>
2021-02-24 15:14:54 +0000

    [QAC-23] Encryption implementation continued (SF)

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -126,10 +126,10 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
       if (booking.isDefined) {
         MovieDAO.totalPrice(booking.get) map { price =>
           PaymentForm.submitForm.bindFromRequest().fold({ formWithErrors =>
-            BadRequest(views.html.payment(PaymentForm.submitForm.fill(Payment(0,"", 0, "", 0, booking.get.id)), price))
+            BadRequest(views.html.payment(PaymentForm.submitForm.fill(Payment(0,"", "", "", "", booking.get.id)), price))
           }, { widget =>
             println("form complete")
-            createP(widget)
+            createP(Payment(0, widget.cardHolderName, sha1(widget.cardNo), sha1(widget.expiryDate), sha1(widget.securityCode), widget.bookingID))
             Redirect("/bookingcomplete/" + booking.get.id)
           })
         }

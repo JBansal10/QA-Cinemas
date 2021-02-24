c0315292ac60d149ed830d29b258049754f616c0
Jbansal10 <jbansal@academytrainee.com>
2021-02-24 09:32:55 +0000

    Testing complete on email

--- test/Persistence/EmailTest.scala
+++ test/Persistence/EmailTest.scala
@@ -4,14 +4,18 @@ import Persistence.Domain.EmailOBJ._
 import org.scalatest.flatspec.AsyncFlatSpec
 import org.scalatest.matchers.should.Matchers
 
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
 class EmailTest extends AsyncFlatSpec with Matchers{
 
   behavior of "Email backend"
 
   it should "send an email" in {
-    // TODO need an actual test, tried to implement one but was too challenging
-    emailing(Email("back@end.com", "Back End", "Testing"))
-    assert(true)
+    val email: Email = new Email("back@end.com", "Back End", "Testing")
+    val result = Await.result(emailing(email), Duration.Inf)
+    assert(result === "Email successfully sent!")
+
   }
 
 }
